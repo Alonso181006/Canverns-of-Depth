2d10546285180e7340fc105d2d931fb1f8e1a7da
Alonso181006 <alonsomartin181006@gmail.com>
2023-01-14 20:56:45 -0600

    player attraction

--- test.js
+++ test.js
@@ -1,80 +1,84 @@
 let player, player_right, player_left, player_up, player_down;
-let speed = 20;
+let crab, crab_idle;
+
 
 
 function preload(){
-  human_right = loadAnimation(
+  player_right = loadAnimation(
+    "gameSprites/humanSprites/humanWalk/WBR.png",
+    { frameSize: [32, 32], frames: 4 });
+  player_left = loadAnimation(
     "gameSprites/humanSprites/humanWalk/WBL.png",
     { frameSize: [32, 32], frames: 4 });
 
+  player_up = loadAnimation(
+    "gameSprites/humanSprites/humanWalk/WTL.png",
+    { frameSize: [32, 32], frames: 4 });
 
-  // human_right = loadAnimation(
-  //   "gameSprites/humanSprites/humanWalk/tile000.png",
-  //   "gameSprites/humanSprites/humanWalk/tile001.png",
-  //   "gameSprites/humanSprites/humanWalk/tile002.png",
-  //   "gameSprites/humanSprites/humanWalk/tile003.png");
-
-  // human_right = loadAnimation(
-  //   "gameSprites/humanSprites/humanWalk/tile000.png",
-  //   "gameSprites/humanSprites/humanWalk/tile001.png",
-  //   "gameSprites/humanSprites/humanWalk/tile002.png",
-  //   "gameSprites/humanSprites/humanWalk/tile003.png");
-
-  // human_up = loadAnimation(
-  //   "gameSprites/humanSprites/humanWalk/tile000.png",
-  //   "gameSprites/humanSprites/humanWalk/tile001.png",
-  //   "gameSprites/humanSprites/humanWalk/tile002.png",
-  //   "gameSprites/humanSprites/humanWalk/tile003.png");
+  player_down = loadAnimation(
+    "gameSprites/humanSprites/humanWalk/WTR.png",
+    { frameSize: [32, 32], frames: 4 });
+  crab_idle = loadAnimation(
+    "gameSprites/Crab Enemy Camacebra Games/Idle/Crab1.png",
+    "gameSprites/Crab Enemy Camacebra Games/Idle/Crab2.png",
+    "gameSprites/Crab Enemy Camacebra Games/Idle/Crab3.png",
+    "gameSprites/Crab Enemy Camacebra Games/Idle/Crab4.png",
+    "gameSprites/Crab Enemy Camacebra Games/Idle/Crab5.png"
+  );
 }
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  player = new human_Sprite(400,400, 400);
-  player.addAni("idle", "gameSprites/humanSprites/humanWalk/WBL.png",{ frameSize: [32, 32], frames: 4 });
+  crab = new Sprite(width/2, height/2,32, 32 );
+  crab.addAni("idle", crab_idle);
+  crab.friction = 2;
+  player = new Sprite(400,400, 32, 32);
+  player.addAni("right", player_right);
+  player.addAni("left", player_left);
+  player.addAni("up", player_up);
+  player.addAni("down", player_down);
+
+  crab.attractionPoint(0.1, player.position.x, player.position.y);
 
 }
 
 function draw(){
-  clear();
-  if (kb.pressing("left")) {
-    player.ani = "fly";
-    player.vel.x = -2;
-    player.mirror.x = true;
-  }
-  else if (kb.pressing("right")) {
-    player.ani = "fly";
-    player.vel.x = 2;
-    player.mirror.x = false;
-  }
-  else {
-    player.ani = "idle";
-    player.vel.x = 0;
-  }
+  playerMovement();
+  player.rotation  = 0;
+  player.friction = 4;
+  crab.friction = 4;
+  crab.attractionPoint(0.1, player.position.x, player.position.y);
+  crab.rotation = 0;
+
 
 }
 
-// function keyPressed(){
-//   human_Sprite.animation.stop();
-//   if(kb.pressing("up")){
-//     human_Sprite.position.y -= speed;
-//     human_Sprite.changeAnimation("up"); 
-//     human_Sprite.animation.play();
-//   }
-//   if(kb.pressing("up")){
-//     human_Sprite.position.y -= speed;
-//     human_Sprite.changeAnimation("up"); 
-//     human_Sprite.animation.play();
-//   }
-//   if(kb.pressing("up")){
-//     human_Sprite.position.y -= speed;
-//     human_Sprite.changeAnimation("up"); 
-//     human_Sprite.animation.play();
-//   }
-//   if(kb.pressing("up")){
-//     human_Sprite.position.y -= speed;
-//     human_Sprite.changeAnimation("up"); 
-//     human_Sprite.animation.play();
-//   }
-
-// }
+function playerMovement(){
+  clear();
+  if (kb.pressing("left")) {
+    player.ani = "left";
+    player.ani.scale = 2.5;
+    player.vel.x = -2;
+  }
+  else if (kb.pressing("right")) {
+    player.ani = "right";
+    player.ani.scale = 2.5;
+    player.vel.x = 2;
+  }
+  else if (kb.pressing("up")) {
+    player.ani = "up";
+    player.ani.scale = 2.5;
+    player.vel.y = -2;
+  }
+  else if (kb.pressing("down")) {
+    player.ani = "down";
+    player.ani.scale = 2.5;
+    player.vel.y = 2;
+  }
+  else {
+    player.ani.scale = 2.5;
+    player.vel.x = 0;
+    player.vel.y =0;
+  }
+}
 
 
